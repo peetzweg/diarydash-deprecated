07df8bef8f8d02cf2ff869b35813f043aec53bf8
Phil Czek <phil.czek@gmail.com>
2017-01-15 10:04:08 +0100

    Implements mechanism to port LocalStorage entries to new IndexedDB method

--- src/js/db/DBActions.js
+++ src/js/db/DBActions.js
@@ -12,73 +12,114 @@ class DBActions extends EventEmitter {
 		this.db = null;
 		this.todaysEntry = null;
 
-		const openRequest = indexedDB.open("diarydash", 2);
+		const openRequest = indexedDB.open("diarydash", 1);
 		openRequest.onupgradeneeded = e => {
-			console.log("running onupgradeneeded");
+			console.log("Upgrade needed...");
 			this.db = e.target.result;
 
 			if (!this.db.objectStoreNames.contains("entry")) {
+				console.log("Creating "entry" object store...");
 				this.db.createObjectStore("entry", {keyPath: "date"});
 			}
-
 		};
 
 		openRequest.onsuccess = e => {
-			console.log("Success!");
+			console.log("Database successfully arranged");
 			this.db = e.target.result;
+
+			this.transferLocalStorageToIndexDB();
 			this.emit("ready");
 		};
 
 		openRequest.onerror = e => {
-			console.log("Error");
+			console.log("Error while setting up the database");
 			console.dir(e);
 		};
 	}
 
+	transferLocalStorageToIndexDB() {
+		console.log("Importing old LocalStorage entries");
+		let entries = [];
+		for (let n = 0; n < localStorage.length; n++) {
+			const key = localStorage.key(n);
+			if (key.startsWith("dd_")) {
+				if (localStorage[key].replace("\n", "").trim().length > 0) {
+					entries.push({
+						date: moment(key.replace("dd_", "")).startOf("day").toISOString(),
+						text: localStorage[key]
+					})
+				}
+			}
+		}
+		this.putEntries(entries)
+	}
+
+	putEntries(entries) {
+		const entryStore = this.db.transaction("entry", "readwrite").objectStore("entry");
+
+		let i = 0;
+		putNext();
+
+		function putNext() {
+			if (i < entries.length) {
+				console.log("inserting", entries[i]);
+				entryStore.put(entries[i]).onsuccess = putNext;
+				++i;
+			} else {   // complete
+				console.log("Successfully ported LocalStorage entries");
+				// callback();
+			}
+		}
+	}
 
 	save(editorState) {
 		const dateKey = moment().startOf("day").toISOString();
 		const entry = {
-			text: editorState.getCurrentContent().getPlainText(),
 			date: dateKey,
+			text: editorState.getCurrentContent().getPlainText(),
 		};
+
+		this.saveOrUpdateEntry(entry)
+	}
+
+
+	saveOrUpdateEntry(entry) {
+		console.log("trying to store:", entry);
 		const entryStore = this.db.transaction("entry", "readwrite").objectStore("entry");
 
-		const requestGet = entryStore.get(dateKey);
+		const requestGet = entryStore.get(entry.date);
 		requestGet.onsuccess = ()=> {
 			const requestPut = entryStore.put(entry);
 			requestPut.onsuccess = ()=> {
-				console.log("UPDATED");
+				console.log("updated", entry);
 				this.emit("saved")
 			};
 			requestPut.onError = ()=> {
-				console.log("Error while updating...");
+				console.log("Error while updating:", entry);
 			};
 		};
 		requestGet.onerror = ()=> {
 			const requestAdd = entryStore.add(entry);
 			requestAdd.onsuccess = ()=> {
-				console.log("SAVED");
+				console.log("stored", entry);
 				this.emit("saved")
 			};
 			requestAdd.onerror = ()=> {
-				console.log("Error white Saving...");
+				console.log("Error while storing:", entry);
 			};
-		}
-
-
+		};
 	}
 
 	restoreTodaysEntry() {
 		const entryStore = this.db.transaction("entry").objectStore("entry");
 		const request = entryStore.get(moment().startOf("day").toISOString());
 		request.onsuccess = event => {
-			console.log('event', event);
+			console.log("event", event);
 			this.todaysEntry = request.result;
 			this.emit("restored")
 		};
 		request.onerror = event => {
-			console.log('event', event);
+			console.log("event", event);
 			this.emit("error")
 		}
 	}
